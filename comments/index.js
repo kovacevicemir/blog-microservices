bc2afd776d16b9327e80a568fa83e35e1167487e
kovacevicemir <emir.kovacevic@jjryan.com.au>
2021-02-13 16:14:39 +1000

    setting event bus - emitting events & recieving events

--- comments/index.js
+++ comments/index.js
@@ -1,35 +1,49 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const {randomBytes} = require("crypto");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 //fake database
-const commentsByPostId = {
+const commentsByPostId = {};
 
-};
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.get("/posts/:id/comments",(req,res)=>{
-    res.send(commentsByPostId[req.params.id] || [])
+app.post("/posts/:id/comments", async (req, res) => {
+  //random id
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post("/events",(req,res)=>{
+    console.log(req.body.type)
+
+    res.send({});
 })
 
-app.post("/posts/:id/comments",(req,res)=>{
-    //random id
-    const commentId = randomBytes(4).toString("hex");
-    const {content} = req.body;
-
-    const comments = commentsByPostId[req.params.id] || [];
-
-    comments.push({id:commentId,content});
-
-    commentsByPostId[req.params.id] = comments;
-
-    res.status(201).send(comments);
-})
-
-app.listen(4001,()=>{
-    console.log("Listening on 4001 [comments service]")
-})
+app.listen(4001, () => {
+  console.log("Listening on 4001 [comments service]");
+});
--- posts/index.js
+++ posts/index.js
@@ -1,31 +1,47 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const {randomBytes} = require("crypto");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 //fake database for practice
-const posts = {}
+const posts = {};
 
-app.get("/posts", (req,res)=>{
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post("/posts",(req,res)=>{
-    //random id
-    const id = randomBytes(4).toString("hex");
-    const {title} = req.body;
+app.post("/posts", async (req, res) => {
+  //random id
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = {
-        id,title
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    res.status(201).send(posts[id]);
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events",(req,res)=>{
+    console.log(req.body.type)
+
+    res.send({});
 })
 
-app.listen(4000, ()=>{
-    console.log('Listening on 4000 [posts service]');
-})
+app.listen(4000, () => {
+  console.log("Listening on 4000 [posts service]");
+});
