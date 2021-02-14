386d4a307dcccca803409549149aa915b2203631
kovacevicemir <emir.kovacevic@jjryan.com.au>
2021-02-14 14:22:36 +1000

    Adding Event Syncing action - if one of service fails and comes back online it will pick up all events and build on top of them

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,10 +5,11 @@ const axios = require("axios")
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events",(req,res)=>{
     const event = req.body;
-    const {data} = req.body;
-    console.log(data) 
+    events.push(event)
 
     axios.post("http://localhost:4000/events",event);
     axios.post("http://localhost:4001/events",event);
@@ -16,6 +17,10 @@ app.post("/events",(req,res)=>{
     axios.post("http://localhost:4003/events",event);
 
     res.send({status:"OK"});
+})
+
+app.get("/events",(req,res)=>{
+    res.send(events)
 })
 
 app.listen(4005,()=>{
